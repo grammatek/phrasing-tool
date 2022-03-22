83f54313acd20b2bb017b54d364918bddfe91721
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-22 18:30:54 +0000

    add commandline to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='phrasing-tool',
+	name='text-cleaner',
 	version='0.1.0',
-	description='A tool for adding pauses to text based on phrases',
+	description='A phrasing tool that inserts pause tags into text based on phrases',
 	author='Helga Svala Sigurðardóttir',
 	author_email='helgas@ru.is',
-	url='https://github.com/grammatek/phrasing-tool',
-	packages=['phrasing'],
+	url='https://github.com/grammatek/text-cleaner',
+	packages=find_packages(),
 	install_requires=[
 		'setuptools',
 	],
 	python_requires='>=3.5',
-
+	entry_points={
+			'console_scripts': [
+				'phrasing=phrasing.phrasing:main'
+							]
+				}
 	)
+
