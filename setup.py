8cab2401900ba785be82eedb5b9ca4d17593474e
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-22 09:09:09 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='phrasing-tool',
+	version='0.1.0',
+	description='A tool for adding pauses to text based on phrases',
+	author='Helga Svala Sigurðardóttir',
+	author_email='helgas@ru.is',
+	url='https://github.com/grammatek/phrasing-tool',
+	packages=find_packages(),
+	install_requires=[
+		'setuptools',
+	],
+	python_requires='>=3.5',
+
+	)
